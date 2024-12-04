e8021358e265236cae49615033c988eb064c6886
NEEPATHA <neelesh.pathak@abirnetworks.com>
2024-12-04 10:51:50 +0530

    Add files via upload

---
+++ nestedObjects.js
@@ -0,0 +1,14 @@
+const person ={
+  name:'Neelesh',
+  age:25,
+  address:{
+    street:'Btm Layout 2nd Stage',
+    city:'Bangalore',
+    country:'India',
+  },
+  hobby:['Reading','Coding']
+}
+
+console.log(person.name);
+console.log(person.address.city);
+console.log(person.hobby[0]);
